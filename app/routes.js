efeed8a6afa2c6c04fab0abf785f38b07e8dfaa8
Helen Spires <h.spiresnhs.net@nhsbsamminsmbp4.lan>
2020-06-15 22:48:14 +0100

    added current folder for iteration, tidied sign in pages

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const router = express.Router();
 
-// Add your routes here - above the module.exports line
 
+// Start folder specific routes
+router.use('/alpha', require('./views/alpha/_routes'));
+// current sprint, remember to add older sprint when adding a new folder!
+router.use('/current', require('./views/current/_routes'));
+
+// Add your routes here - above the module.exports line
 module.exports = router;
